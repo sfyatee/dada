37d35f51622b98ded98c8a0267aa1bef5c388620
demian garcia <dag@demians.org>
2026-03-13 14:29:06 -0700

    lex: flush out structure

--- lex/lex.go
+++ lex/lex.go
@@ -1,28 +1,2 @@
 package lex
 
-import (
-    "fmt"
-    "strings"
-)
-
-type Error string
-
-func (e Error) Error() string { return string(e) }
-
-type EOF string
-
-func (e EOF) Error() string { return string(e) }
-
-type tokenType int
-
-const (
-    tokenError tokenType = iota
-    tokenEOF
-    tokenLpar
-    tokenRpar
-    tokenDot
-    tokenQuote
-    tokenAtom
-    tokenConst
-    tokenNumber
-)
---
+++ lex/token.go
@@ -0,0 +1,28 @@
+package lex
+
+import (
+    "fmt"
+    "strings"
+)
+
+type Error string
+
+func (e Error) Error() string { return string(e) }
+
+type EOF string
+
+func (e EOF) Error() string { return string(e) }
+
+type tokenType int
+
+const (
+    tokenError tokenType = iota
+    tokenEOF
+    tokenLpar
+    tokenRpar
+    tokenDot
+    tokenQuote
+    tokenAtom
+    tokenConst
+    tokenNumber
+)
