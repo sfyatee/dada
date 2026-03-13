c886d23657393bf1ab6e9ee5f7e9dcb6359d927d
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2026-03-13 14:48:40 -0700

    lex/token.go: add missing String helper and tokQuote

--- lex/token.go
+++ lex/token.go
@@ -32,6 +32,37 @@ type token struct {
     text string
 }
 
+// Our parser has support for . an quote (`). Added for .
+var tokQuote = &token{
+    typ:  tokenAtom,
+    text: "quote",
+}
+
+func (tt tokenType) String() string {
+    switch tt {
+    case tokenError:
+        return "error"
+    case tokenEOF:
+        return "eof"
+    case tokenLpar:
+        return "("
+    case tokenRpar:
+        return ")"
+    case tokenDot:
+        return "."
+    case tokenQuote:
+        return "'"
+    case tokenAtom:
+        return "atom"
+    case tokenConst:
+        return "const"
+    case tokenNumber:
+        return "number"
+    default:
+        return fmt.Sprintf("tokenType(%d)", int(tt))
+    }
+}
+
 func (t *token) String() string {
     if t == nil {
         return "<nil>"
