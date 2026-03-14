0424067f5ff1534f2a9b80a92bb0c8f1285045f9
demian garcia <dag@demians.org>
2026-03-13 21:38:47 -0700

    lex: fix and complete tokenizer

--- lex/lex.go
+++ lex/lex.go
@@ -1,2 +1,215 @@
 package lex
 
+import (
+	"bufio"
+	"io"
+	"unicode"
+)
+
+type lexer struct {
+	r *bufio.Reader
+}
+
+func newLexer(rr io.RuneReader) *lexer {
+	if br, ok := rr.(*bufio.Reader); ok {
+		return &lexer{r: br}
+	}
+	return &lexer{r: bufio.NewReader(runeReaderAdapter{rr: rr})}
+}
+
+type runeReaderAdapter struct {
+	rr io.RuneReader
+}
+
+func (a runeReaderAdapter) Read(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+	r, _, err := a.rr.ReadRune()
+	if err != nil {
+		return 0, err
+	}
+	return copy(p, string(r)), nil
+}
+
+func (l *lexer) next() *token {
+	l.skipSpace()
+
+	r, err := l.readRune()
+	if err == io.EOF {
+		return &token{typ: tokenEOF, text: "EOF"}
+	}
+	if err != nil {
+		panic(Error(err.Error()))
+	}
+
+	switch r {
+	case '(':
+		return &token{typ: tokenLpar, text: "("}
+	case ')':
+		return &token{typ: tokenRpar, text: ")"}
+	case '.':
+		return &token{typ: tokenDot, text: "."}
+	case '\'':
+		return &token{typ: tokenQuote, text: "'"}
+	}
+
+	if isDigit(r) || (r == '-' && l.peekDigit()) {
+		return l.lexNumber(r)
+	}
+
+	if isAtomStart(r) {
+		return l.lexAtomOrConst(r)
+	}
+
+	panic(Error("invalid character: " + string(r)))
+}
+
+func (l *lexer) skipSpace() rune {
+	for {
+		r, err := l.readRune()
+		if err == io.EOF {
+			return 0
+		}
+		if err != nil {
+			panic(Error(err.Error()))
+		}
+		if !unicode.IsSpace(r) {
+			if err := l.r.UnreadRune(); err != nil {
+				panic(Error(err.Error()))
+			}
+			return r
+		}
+	}
+}
+
+func (l *lexer) skipToNewline() {
+	for {
+		r, err := l.readRune()
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			panic(Error(err.Error()))
+		}
+		if r == '\n' {
+			return
+		}
+	}
+}
+
+func (l *lexer) lexNumber(first rune) *token {
+	rs := []rune{first}
+
+	if first == '-' {
+		r, err := l.readRune()
+		if err != nil {
+			panic(Error("bad number"))
+		}
+		if !isDigit(r) {
+			panic(Error("bad number"))
+		}
+		rs = append(rs, r)
+	}
+
+	for {
+		r, err := l.readRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(Error(err.Error()))
+		}
+		if !isDigit(r) {
+			if err := l.r.UnreadRune(); err != nil {
+				panic(Error(err.Error()))
+			}
+			break
+		}
+		rs = append(rs, r)
+	}
+
+	return &token{
+		typ:  tokenNumber,
+		text: string(rs),
+	}
+}
+
+func (l *lexer) lexAtomOrConst(first rune) *token {
+	rs := []rune{first}
+
+	for {
+		r, err := l.readRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(Error(err.Error()))
+		}
+		if !isAtomPart(r) {
+			if err := l.r.UnreadRune(); err != nil {
+				panic(Error(err.Error()))
+			}
+			break
+		}
+		rs = append(rs, r)
+	}
+
+	s := string(rs)
+	if isReservedConst(s) {
+		return &token{
+			typ:  tokenConst,
+			text: s,
+		}
+	}
+
+	return &token{
+		typ:  tokenAtom,
+		text: s,
+	}
+}
+
+func (l *lexer) peekDigit() bool {
+	r, err := l.readRune()
+	if err != nil {
+		return false
+	}
+	if err := l.r.UnreadRune(); err != nil {
+		panic(Error(err.Error()))
+	}
+	return isDigit(r)
+}
+
+func (l *lexer) readRune() (rune, error) {
+	r, _, err := l.r.ReadRune()
+	return r, err
+}
+
+func isDigit(r rune) bool {
+	return '0' <= r && r <= '9'
+}
+
+func isAtomStart(r rune) bool {
+	if unicode.IsLetter(r) || r == '_' {
+		return true
+	}
+	switch r {
+	case '+', '-', '*', '/', '<', '>', '=', '?', '!':
+		return true
+	default:
+		return false
+	}
+}
+
+func isAtomPart(r rune) bool {
+	return isAtomStart(r) || isDigit(r)
+}
+
+func isReservedConst(s string) bool {
+	switch s {
+	case "true", "false", "unit", "nil":
+		return true
+	default:
+		return false
+	}
+}
--- lex/token.go
+++ lex/token.go
@@ -1,8 +1,8 @@
 package lex
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 type Error string
@@ -16,66 +16,60 @@ func (e EOF) Error() string { return string(e) }
 type tokenType int
 
 const (
-    tokenError tokenType = iota
-    tokenEOF
-    tokenLpar
-    tokenRpar
-    tokenDot
-    tokenQuote
-    tokenAtom
-    tokenConst
-    tokenNumber
+	tokenError tokenType = iota
+	tokenEOF
+	tokenLpar
+	tokenRpar
+	tokenDot
+	tokenQuote
+	tokenAtom
+	tokenConst
+	tokenNumber
 )
 
-type token struct {
-    typ  tokenType
-    text string
-}
-
-// Our parser has support for . an quote (`). Added for .
-var tokQuote = &token{
-    typ:  tokenAtom,
-    text: "quote",
-}
-
 func (tt tokenType) String() string {
-    switch tt {
-    case tokenError:
-        return "error"
-    case tokenEOF:
-        return "eof"
-    case tokenLpar:
-        return "("
-    case tokenRpar:
-        return ")"
-    case tokenDot:
-        return "."
-    case tokenQuote:
-        return "'"
-    case tokenAtom:
-        return "atom"
-    case tokenConst:
-        return "const"
-    case tokenNumber:
-        return "number"
-    default:
-        return fmt.Sprintf("tokenType(%d)", int(tt))
-    }
+	switch tt {
+	case tokenError:
+		return "error"
+	case tokenEOF:
+		return "EOF"
+	case tokenLpar:
+		return "("
+	case tokenRpar:
+		return ")"
+	case tokenDot:
+		return "."
+	case tokenQuote:
+		return "'"
+	case tokenAtom:
+		return "atom"
+	case tokenConst:
+		return "const"
+	case tokenNumber:
+		return "number"
+	default:
+		return fmt.Sprintf("tokenType(%d)", int(tt))
+	}
+}
+
+type token struct {
+	typ  tokenType
+	text string
 }
 
 func (t *token) String() string {
-    if t == nil {
-        return "<nil>"
-    }
-    if t.text != "" {
-        return t.text
-    }
-    return t.typ.String()
+	if t == nil {
+		return "<nil>"
+	}
+	if t.text != "" {
+		return t.text
+	}
+	return t.typ.String()
 }
 
 func (t *token) buildString(b *strings.Builder) {
-    if t == nil {
-        return
-    }
-    b.WriteString(t.String())
+	if t == nil {
+		return
+	}
+	b.WriteString(t.String())
 }
