74b4752ab698485215dd0a20e16e9198b754fe95
DylanOseida <dylan.oseida.103@my.csun.edu>
2026-05-14 15:53:53 -0700

    dada.go: wired dada.go into parser and code generation pipeline

--- dada.go
+++ dada.go
@@ -1,8 +1,88 @@
 package main
 
-//import (
-	//"flag"
-	//"os"
-//)
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
 
-func main() {}
+	"dada/gen"
+	"dada/lex"
+	"dada/parse"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	outPath := flag.String("o", "", "output JavaScript file")
+	flag.Parse()
+
+	var input io.Reader
+
+	switch flag.NArg() {
+	case 0:
+		input = os.Stdin
+
+	case 1:
+		f, err := os.Open(flag.Arg(0))
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		input = f
+
+	default:
+		return fmt.Errorf("usage: dada [-o output.js] [file]")
+	}
+
+	prog, err := parseInput(input)
+	if err != nil {
+		return err
+	}
+
+	var output io.Writer = os.Stdout
+
+	if *outPath != "" {
+		f, err := os.Create(*outPath)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		output = f
+	}
+
+	return gen.Generate(output, prog)
+}
+
+func parseInput(r io.Reader) (prog *parse.Program, err error) {
+	defer func() {
+		if v := recover(); v != nil {
+			switch e := v.(type) {
+			case lex.Error:
+				err = fmt.Errorf("parse error: %s", e)
+
+			case lex.EOF:
+				err = fmt.Errorf("unexpected EOF: %s", e)
+
+			case error:
+				err = e
+
+			default:
+				panic(v)
+			}
+
+			prog = nil
+		}
+	}()
+
+	p := parse.NewParser(bufio.NewReader(r))
+	return p.ParseProgram(), nil
+}
