7ea7d8073a472c3d57020bd4d9ca6abec5219792
DylanOseida <dylan.oseida.103@my.csun.edu>
2026-05-14 19:28:48 -0700

    check/check.go: implement function, lambda, block, and mutable variable checking

--- check/check.go
+++ check/check.go
@@ -9,19 +9,41 @@ func (e Error) Error() string {
 }
 
 type TypeEnv struct {
-	parent *TypeEnv
-	vars   map[string]parse.Type
+	parent  *TypeEnv
+	vars    map[string]parse.Type
+	mutable map[string]bool
+}
+
+type Binding struct {
+	Type    parse.Type
+	Mutable bool
 }
 
 func NewTypeEnv(parent *TypeEnv) *TypeEnv {
 	return &TypeEnv{
-		parent: parent,
-		vars:   map[string]parse.Type{},
+		parent:  parent,
+		vars:    map[string]parse.Type{},
+		mutable: map[string]bool{},
 	}
 }
 
 func (e *TypeEnv) Bind(name string, typ parse.Type) {
 	e.vars[name] = typ
+}
+
+func (e *TypeEnv) BindMutable(name string, typ parse.Type) {
+	e.vars[name] = typ
+	e.mutable[name] = true
+}
+
+func (e *TypeEnv) IsMutable(name string) bool {
+	for cur := e; cur != nil; cur = cur.parent {
+		if _, ok := cur.vars[name]; ok {
+			return cur.mutable[name]
+		}
+	}
+
+	return false
 }
 
 func (e *TypeEnv) Lookup(name string) (parse.Type, bool) {
@@ -83,6 +105,18 @@ func (c *Checker) checkProgram(program *parse.Program) error {
 		}
 	}
 
+	for _, fn := range program.FuncDefs {
+		if err := c.checkFuncDef(fn); err != nil {
+			return err
+		}
+	}
+
+	if program.Expr != nil {
+		env := NewTypeEnv(nil)
+		if _, err := c.checkExpr(env, program.Expr); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -403,6 +437,19 @@ func (c *Checker) checkExpr(env *TypeEnv, expr parse.Expr) (parse.Type, error) {
 
 	case parse.OpExpr:
 		return c.checkOpExpr(env, e)
+
+	case parse.LambdaExpr:
+		return c.checkLambdaExpr(env, e)
+
+	case parse.CallExpr:
+		return c.checkCallExpr(env, e)
+
+	case parse.CallHOFExpr:
+		return c.checkCallHOFExpr(env, e)
+
+	case parse.BlockExpr:
+		return c.checkBlockExpr(env, e)
+
 	}
 
 	return nil, Error("unsupported expression")
@@ -456,4 +503,165 @@ func (c *Checker) checkOpExpr(env *TypeEnv, expr parse.OpExpr) (parse.Type, erro
 	}
 
 	return nil, Error("unknown operator")
+}
+
+func (c *Checker) checkFuncDef(fn *parse.FuncDef) error {
+	env := NewTypeEnv(nil)
+
+	for _, p := range fn.Params {
+		env.Bind(p.Name, p.Type)
+	}
+
+	bodyType, err := c.checkExpr(env, fn.Body)
+	if err != nil {
+		return err
+	}
+
+	if !equalTypes(bodyType, fn.ReturnType) {
+		return Error("function return type mismatch")
+	}
+
+	return nil
+}
+
+func (c *Checker) checkCallExpr(env *TypeEnv, expr parse.CallExpr) (parse.Type, error) {
+	sig, ok := c.funcs[expr.Name]
+	if !ok {
+		return nil, Error("unknown function")
+	}
+
+	sig = instantiateFuncSig(sig)
+
+	if len(expr.Args) != len(sig.Params) {
+		return nil, Error("wrong number of function arguments")
+	}
+
+	subst := Subst{}
+
+	for i, arg := range expr.Args {
+		argType, err := c.checkExpr(env, arg)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := unify(sig.Params[i], argType, subst); err != nil {
+			return nil, Error("function argument type mismatch")
+		}
+	}
+
+	return applySubst(sig.Return, subst), nil
+}
+
+func (c *Checker) checkCallHOFExpr(env *TypeEnv, expr parse.CallHOFExpr) (parse.Type, error) {
+	fnType, err := c.checkExpr(env, expr.Fn)
+	if err != nil {
+		return nil, err
+	}
+
+	ft, ok := fnType.(parse.FuncType)
+	if !ok {
+		return nil, Error("callhof expected function type")
+	}
+
+	if len(expr.Args) != len(ft.Params) {
+		return nil, Error("wrong number of callhof arguments")
+	}
+
+	subst := Subst{}
+
+	for i, arg := range expr.Args {
+		argType, err := c.checkExpr(env, arg)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := unify(ft.Params[i], argType, subst); err != nil {
+			return nil, Error("callhof argument type mismatch")
+		}
+	}
+
+	return applySubst(ft.Return, subst), nil
+}
+
+func (c *Checker) checkLambdaExpr(env *TypeEnv, expr parse.LambdaExpr) (parse.Type, error) {
+	child := NewTypeEnv(env)
+
+	var params []parse.Type
+
+	for _, p := range expr.Params {
+		params = append(params, p.Type)
+		child.Bind(p.Name, p.Type)
+	}
+
+	bodyType, err := c.checkExpr(child, expr.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return parse.FuncType{
+		Params: params,
+		Return: bodyType,
+	}, nil
+}
+
+func (c *Checker) checkBlockExpr(env *TypeEnv, expr parse.BlockExpr) (parse.Type, error) {
+	child := NewTypeEnv(env)
+
+	for _, stmt := range expr.Stmts {
+		if err := c.checkStmt(child, stmt); err != nil {
+			return nil, err
+		}
+	}
+
+	return c.checkExpr(child, expr.Expr)
+}
+
+func (c *Checker) checkStmt(env *TypeEnv, stmt parse.Stmt) error {
+	switch s := stmt.(type) {
+
+	case parse.ValStmt:
+		exprType, err := c.checkExpr(env, s.Expr)
+		if err != nil {
+			return err
+		}
+
+		env.Bind(s.Name, exprType)
+		return nil
+
+	case parse.VarStmt:
+		exprType, err := c.checkExpr(env, s.Expr)
+		if err != nil {
+			return err
+		}
+
+		if !equalTypes(exprType, s.Type) {
+			return Error("variable type mismatch")
+		}
+
+		env.BindMutable(s.Name, s.Type)
+		return nil
+
+	case parse.AssignStmt:
+		oldType, ok := env.Lookup(s.Name)
+		if !ok {
+			return Error("assignment to unbound variable")
+		}
+
+		if !env.IsMutable(s.Name) {
+			return Error("assignment to immutable variable")
+		}
+
+		exprType, err := c.checkExpr(env, s.Expr)
+		if err != nil {
+			return err
+		}
+
+		if !equalTypes(oldType, exprType) {
+			return Error("assignment type mismatch")
+		}
+
+		return nil
+	}
+
+	return Error("unsupported statement")
 }
