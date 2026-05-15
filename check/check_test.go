77faae3d3742181282648c335865a621a18c4df6
DylanOseida <dylan.oseida.103@my.csun.edu>
2026-05-14 20:06:57 -0700

    check/check_test.go: add constructor checking tests

--- check/check_test.go
+++ check/check_test.go
@@ -15,17 +15,23 @@ func TestCheckerExists(t *testing.T) {
 }
 
 func TestDuplicateFunctionFails(t *testing.T) {
-	_ = t
-}
+	prog := &parse.Program{
+		FuncDefs: []*parse.FuncDef{
+			{
+				Name:       "f",
+				ReturnType: parse.IntType{},
+				Body:       parse.IntExpr{Value: 1},
+			},
+			{
+				Name:       "f",
+				ReturnType: parse.IntType{},
+				Body:       parse.IntExpr{Value: 2},
+			},
+		},
+	}
 
-func TestEqualTypes(t *testing.T) {
-	a := equalTypes(
-		parse.IntType{},
-		parse.IntType{},
-	)
-
-	if !a {
-		t.Fatalf("expected equal")
+	if err := Check(prog); err == nil {
+		t.Fatalf("expected error")
 	}
 }
 
@@ -370,5 +376,276 @@ func TestCheckRejectsAssignmentTypeMismatch(t *testing.T) {
 
 	if err == nil {
 		t.Fatalf("expected error")
+	}
+}
+
+func TestCheckConstructorExpr(t *testing.T) {
+	prog := &parse.Program{
+		AlgDefs: []*parse.AlgDef{
+			{
+				Name:     "List",
+				TypeVars: []string{"A"},
+				ConsDefs: []*parse.ConsDef{
+					{
+						Name: "lcons",
+						Args: []parse.Type{
+							parse.TypeVar{Name: "A"},
+							parse.AlgType{
+								Name: "List",
+								Args: []parse.Type{
+									parse.TypeVar{Name: "A"},
+								},
+							},
+						},
+					},
+					{
+						Name: "lnil",
+					},
+				},
+			},
+		},
+		Expr: parse.ConsExpr{
+			Name: "lcons",
+			Args: []parse.Expr{
+				parse.IntExpr{
+					Value: 1,
+				},
+				parse.ConsExpr{
+					Name: "lnil",
+				},
+			},
+		},
+	}
+
+	if err := Check(prog); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+}
+
+func TestCheckRejectsUnknownConstructor(t *testing.T) {
+	prog := &parse.Program{
+		Expr: parse.ConsExpr{
+			Name: "bad",
+		},
+	}
+
+	if err := Check(prog); err == nil {
+		t.Fatalf("expected error")
+	}
+}
+
+func TestCheckRejectsConstructorArgCountMismatch(t *testing.T) {
+	prog := &parse.Program{
+		AlgDefs: []*parse.AlgDef{
+			{
+				Name: "Box",
+				ConsDefs: []*parse.ConsDef{
+					{
+						Name: "box",
+						Args: []parse.Type{
+							parse.IntType{},
+						},
+					},
+				},
+			},
+		},
+		Expr: parse.ConsExpr{
+			Name: "box",
+		},
+	}
+
+	if err := Check(prog); err == nil {
+		t.Fatalf("expected error")
+	}
+}
+
+func TestCheckRejectsConstructorArgTypeMismatch(t *testing.T) {
+	prog := &parse.Program{
+		AlgDefs: []*parse.AlgDef{
+			{
+				Name: "Box",
+				ConsDefs: []*parse.ConsDef{
+					{
+						Name: "box",
+						Args: []parse.Type{
+							parse.IntType{},
+						},
+					},
+				},
+			},
+		},
+		Expr: parse.ConsExpr{
+			Name: "box",
+			Args: []parse.Expr{
+				parse.BoolExpr{
+					Value: true,
+				},
+			},
+		},
+	}
+
+	if err := Check(prog); err == nil {
+		t.Fatalf("expected error")
+	}
+}
+
+func TestApplySubstFuncType(t *testing.T) {
+	subst := Subst{
+		"A": parse.IntType{},
+		"B": parse.BooleanType{},
+	}
+
+	r := applySubst(
+		parse.FuncType{
+			Params: []parse.Type{
+				parse.TypeVar{Name: "A"},
+			},
+			Return: parse.TypeVar{Name: "B"},
+		},
+		subst,
+	)
+
+	want := parse.FuncType{
+		Params: []parse.Type{parse.IntType{}},
+		Return: parse.BooleanType{},
+	}
+
+	if !equalTypes(r, want) {
+		t.Fatalf("bad function substitution")
+	}
+}
+
+func TestUnifyBooleanAndUnit(t *testing.T) {
+	if err := unify(parse.BooleanType{}, parse.BooleanType{}, Subst{}); err != nil {
+		t.Fatalf("unexpected error")
+	}
+
+	if err := unify(parse.UnitType{}, parse.UnitType{}, Subst{}); err != nil {
+		t.Fatalf("unexpected error")
+	}
+}
+
+func TestUnifyFuncType(t *testing.T) {
+	a := parse.FuncType{
+		Params: []parse.Type{parse.IntType{}},
+		Return: parse.BooleanType{},
+	}
+
+	b := parse.FuncType{
+		Params: []parse.Type{parse.IntType{}},
+		Return: parse.BooleanType{},
+	}
+
+	if err := unify(a, b, Subst{}); err != nil {
+		t.Fatalf("unexpected error")
+	}
+}
+
+func TestLessThanExpr(t *testing.T) {
+	env := NewTypeEnv(nil)
+
+	typ, err := newChecker().checkExpr(
+		env,
+		parse.OpExpr{
+			Op:    "<",
+			Left:  parse.IntExpr{Value: 1},
+			Right: parse.IntExpr{Value: 2},
+		},
+	)
+
+	if err != nil {
+		t.Fatalf("unexpected error")
+	}
+
+	if !equalTypes(typ, parse.BooleanType{}) {
+		t.Fatalf("expected boolean")
+	}
+}
+
+func TestEqualEqualExpr(t *testing.T) {
+	env := NewTypeEnv(nil)
+
+	typ, err := newChecker().checkExpr(
+		env,
+		parse.OpExpr{
+			Op:    "==",
+			Left:  parse.IntExpr{Value: 1},
+			Right: parse.IntExpr{Value: 1},
+		},
+	)
+
+	if err != nil {
+		t.Fatalf("unexpected error")
+	}
+
+	if !equalTypes(typ, parse.BooleanType{}) {
+		t.Fatalf("expected boolean")
+	}
+}
+
+func TestEqualTypes(t *testing.T) {
+	tests := []struct {
+		name string
+		a    parse.Type
+		b    parse.Type
+	}{
+		{
+			name: "int",
+			a:    parse.IntType{},
+			b:    parse.IntType{},
+		},
+		{
+			name: "boolean",
+			a:    parse.BooleanType{},
+			b:    parse.BooleanType{},
+		},
+		{
+			name: "unit",
+			a:    parse.UnitType{},
+			b:    parse.UnitType{},
+		},
+		{
+			name: "type var",
+			a:    parse.TypeVar{Name: "A"},
+			b:    parse.TypeVar{Name: "A"},
+		},
+		{
+			name: "alg type",
+			a: parse.AlgType{
+				Name: "List",
+				Args: []parse.Type{
+					parse.IntType{},
+				},
+			},
+			b: parse.AlgType{
+				Name: "List",
+				Args: []parse.Type{
+					parse.IntType{},
+				},
+			},
+		},
+		{
+			name: "func type",
+			a: parse.FuncType{
+				Params: []parse.Type{
+					parse.IntType{},
+				},
+				Return: parse.BooleanType{},
+			},
+			b: parse.FuncType{
+				Params: []parse.Type{
+					parse.IntType{},
+				},
+				Return: parse.BooleanType{},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if !equalTypes(tt.a, tt.b) {
+				t.Fatalf("expected types to be equal")
+			}
+		})
 	}
 }
